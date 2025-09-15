fea91c1d78d3dffd267e3dbd0dd2482730be8373
JBExcoffier <jb.excoffier@gmail.com>
2025-09-15 17:10:13 +0200

    change env var used for destination database and replication of public schema only

--- replicate.sh
+++ replicate.sh
@@ -36,6 +36,6 @@ BACKUP_NAME=`tar -tf $ARCHIVE_NAME | tail -n 1`
 
 tar -C /app -xvf $ARCHIVE_NAME
 
-pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --verbose --dbname $DATABASE_URL /app$BACKUP_NAME || {
+pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --verbose --schema public --dbname $SCALINGO_POSTGRESQL_URL /app$BACKUP_NAME || {
     echo "pg_restore encountered errors (this is sometimes normal)"
 }
