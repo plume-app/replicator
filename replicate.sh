244cc5dcd19fae5574856135c70558fc9db88d61
JBExcoffier <jb.excoffier@gmail.com>
2025-09-17 09:15:59 +0200

    restoring all tables

--- replicate.sh
+++ replicate.sh
@@ -36,9 +36,6 @@ BACKUP_NAME=`tar -tf $ARCHIVE_NAME | tail -n 1`
 
 tar -C /app -xvf $ARCHIVE_NAME
 
-pg_restore -l /app$BACKUP_NAME > /app/dump.list
-grep -v -E 'ahoy|solid_queue' /app/dump.list > /app/dump_clean.list
-
-pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --verbose --schema public -L /app/dump_clean.list --dbname $SCALINGO_POSTGRESQL_URL /app$BACKUP_NAME || {
+pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --verbose --schema public --dbname $SCALINGO_POSTGRESQL_URL /app$BACKUP_NAME || {
     echo "pg_restore encountered errors (this is sometimes normal)"
 }
