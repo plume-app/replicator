65b6151937b9da620f9c7104c4c55ce8b785cc2b
JBExcoffier <jb.excoffier@gmail.com>
2025-09-24 13:53:07 +0200

    update replicate bash file

--- replicate.sh
+++ replicate.sh
@@ -1,41 +1,115 @@
 #!/bin/bash
 
+
 # Exit when any command fails
 set -e
 
-if [ -z "$SOURCE_APP" ]
-then
-  echo "SOURCE_APP is not set"
-  exit 1
-fi
 
+# Check of env vars
 if [ -z "$SCALINGO_CLI_TOKEN" ]
 then
   echo "SCALINGO_CLI_TOKEN is not set"
   exit 1
 fi
 
-if [ -z "$SCALINGO_POSTGRESQL_URL" ]
+if [ -z "$SCALINGO_ORIGINAL_POSTGRESQL_URL" ]
 then
-  echo "SCALINGO_POSTGRESQL_URL is not set"
+  echo "SCALINGO_ORIGINAL_POSTGRESQL_URL is not set"
   exit 1
 fi
 
+if [ -z "$SCALINGO_DESTINATION_POSTGRESQL_URL" ]
+then
+  echo "SCALINGO_DESTINATION_POSTGRESQL_URL is not set"
+  exit 1
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Env variables checked"
+
+# Install packages
 install-scalingo-cli
 dbclient-fetcher psql
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Packages installed"
+
+
+# Login to Scalingo CLI
 scalingo login --api-token $SCALINGO_CLI_TOKEN
 
-ADDON_ID=`scalingo --app $SOURCE_APP addons | grep postgresql | awk -F ' | ' '{print $4}'`
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Logged in to Scalingo CLI"
 
-ARCHIVE_NAME=backup.tar.gz
 
-scalingo --app $SOURCE_APP --addon $ADDON_ID backups-download --output $ARCHIVE_NAME
+# Dump original database with some tables excluded
+DUMP_NAME=/app/partial_dump.dump
 
-BACKUP_NAME=`tar -tf $ARCHIVE_NAME | tail -n 1`
+pg_dump --clean --if-exists \
+  --format=c \
+  --verbose \
+  --dbname="${SCALINGO_ORIGINAL_POSTGRESQL_URL}" \
+  --no-owner --no-privileges --no-comments \
+  --exclude-schema 'information_schema' \
+  --exclude-schema '^pg_*' \
+  --exclude-table='public.ahoy*' \
+  --exclude-table='public.solid_queue*' \
+  --exclude-table='public.versions' \
+  --file $DUMP_NAME
 
-tar -C /app -xvf $ARCHIVE_NAME
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Original database (partially) dumped"
 
-pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --verbose --schema public --dbname $SCALINGO_POSTGRESQL_URL /app$BACKUP_NAME || {
-    echo "pg_restore encountered errors (this is sometimes normal)"
-}
+
+# Clean public schema and set default privileges
+psql "$SCALINGO_DESTINATION_POSTGRESQL_URL" <<'EOSQL'
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+EOSQL
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Destination database : public schema dropped and recreated"
+
+# Define roles in Bash array
+ROLES="plume_app_d_8501 admin admin_patroni metabase postgresql replicator"
+
+
+# Set schema usage, default privileges, and grants for future objects
+for role in $ROLES; do
+  psql "$SCALINGO_DESTINATION_POSTGRESQL_URL" <<EOSQL
+    GRANT USAGE ON SCHEMA public TO $role;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $role;
+EOSQL
+done
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - All privileges granted before restore"
+
+# Restore database into destination database
+pg_restore --section=pre-data \
+  --clean --if-exists \
+  --no-owner --no-privileges \
+  --verbose \
+  --dbname=$SCALINGO_DESTINATION_POSTGRESQL_URL \
+  $DUMP_NAME
+
+pg_restore --section=data \
+  --no-owner --no-privileges \
+  --disable-triggers \
+  --verbose \
+  --dbname=$SCALINGO_DESTINATION_POSTGRESQL_URL \
+  $DUMP_NAME
+
+pg_restore --section=post-data \
+  --no-owner --no-privileges \
+  --verbose \
+  --dbname=$SCALINGO_DESTINATION_POSTGRESQL_URL \
+  $DUMP_NAME
+
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Restore is complete"
+
+# Grant privileges on all restored tables
+for role in $ROLES; do
+  psql "$SCALINGO_DESTINATION_POSTGRESQL_URL" <<EOSQL
+    GRANT ALL ON ALL TABLES IN SCHEMA public TO $role;
+EOSQL
+done
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - All privileges granted after restore"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Full process is complete"
