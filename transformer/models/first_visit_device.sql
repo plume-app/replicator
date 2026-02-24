60144aeece18c8fdcf3acbb4183bd271c8f06665
JBExcoffier <jb.excoffier@gmail.com>
2026-02-24 16:28:29 +0100

    first visit infos incremental mode

--- transformer/models/first_visit_device.sql
+++ transformer/models/first_visit_device.sql
@@ -1,8 +1,20 @@
-SELECT 
-    DISTINCT ON (user_id)
-    user_id,
-    started_at,
-    device_type
-FROM ahoy_visits
-WHERE user_id IS NOT NULL
-ORDER BY user_id, started_at ASC
+{{
+  config(
+    materialized='incremental',
+    incremental_strategy='delete+insert',
+    unique_key='user_id'
+  )
+}}
+
+SELECT
+    DISTINCT ON (v.user_id)
+    v.user_id,
+    v.started_at,
+    v.device_type
+FROM ahoy_visits v
+{% if is_incremental() %}
+INNER JOIN users u ON u.id = v.user_id
+    AND u.created_at >= current_date - interval '14 days'
+{% endif %}
+WHERE v.user_id IS NOT NULL
+ORDER BY v.user_id, v.started_at ASC
